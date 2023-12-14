f1e7913caf1f3236ad5ad14b12f1e649f0de5f26
Asmr web coding <138141838+AsmrWebCoding@users.noreply.github.com>
2023-12-15 01:14:25 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let text = document.getElementById('text');
+let tele = document.getElementById('tele');
+let moon = document.getElementById('moon');
+let sky = document.getElementById('sky');
+let right = document.getElementById('right');
+let left = document.getElementById('left');
+let header = document.getElementById('header');
